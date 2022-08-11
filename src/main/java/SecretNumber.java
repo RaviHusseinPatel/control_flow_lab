81c39a5f5a1c0e2af464c2499774d7ae46a5037c
Ravi Hussein-Patel <ravihusseinpatel@gmail.com>
2022-08-12 00:27:09 +0100

    (Spelling/Formatting)

--- src/main/java/SecretNumber.java
+++ src/main/java/SecretNumber.java
@@ -2,35 +2,40 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class SecretNumber {
-    //    This is our Psuedo Code, used to guideanyone reading out code through our steps and procceses (cmd/)
+    //    This is our Psuedo Code, used to guide anyone reading our code through our steps and processes (cmd/)
 //    We start by entering 'main' and pressing tab so that InteliJ can complete the rest of our code, this is done for each new class we create
+//    RULES 1.
+    //        Always run the code to check if it works- less time finding issue
+    //      Finish codes with semi colon
     public static void main(String[] args) {
 
 //        We want to create a secret number game, but first let's introduce our user to our game, we can do this by simply entering 'sout'
-//        Then we press tab, IntelliJ will autofill for us, and we print our message within the brackets (in quotations as it is text)
+//        Enter output in brackets (in quotes)
+//        println for line, print for nolnbrk
 
         System.out.println("Hi, welcome to the secret number game");
 
 
-//        Run the code to check if it works.
 //        Select 'view' then 'run byte code' This shows what is fed to the JVM( Java Virtual Machine) to run our program.
 
-//        Now let's start our game. First we have to select a random number, as it is a number not text, or a decimal, or a very big number, we use integer before it
-//        But, to make the game better let's create a genuine random number, that changes each time, so even us as the game creator can play
+//        Now let's start our game. First we have to select a random number
+//        But, to make the game better lets create a genuine random number, that changes each time, so even us as the game creator can play
+//        'Random' is a variable
 
-        Random pear = new Random();  //Random is a class to generate something...random. It is a new clas so we use new
+
+         Random generator1 = new Random();  //
 
 //      As it is a number game, we use an int. Assign our class a title, here it is randomNumber
-//        Entitle the integer randomNumber, as sensible name for our random number
-//        This will be equal to our random class (pear)then .nextInt   nextInt returns a value between 0 and whatever number is between our brackets
+//        Entitle the integer secretNumber, as sensible name for our random secret number
+//        This will be equal to our random class (generator)then .nextInt   nextInt returns a value between 0 and whatever number is between our brackets
 //        Outside the brackets you can put +x  x is any number you want t start from added to 0
 
-        int secretNumber = pear.nextInt(1000) + 1;
+        int secretNumber = generator1.nextInt(1000) + 1;
 
 //        Prompt a user to guess what the secret number is using the same coding as the intro (sout)
 //        To simplify the game at this low level, enter the lowest and greatest number it could be
 
-        while (true) {
+        while (true) {  // while(true) break
             System.out.println("What is the secret number? (2-1000)");
 
 
@@ -72,6 +77,7 @@ public class SecretNumber {
 //   However, in this game we want the game to stop when, and only when the user answers the correctly (in our only if condition)
 //  Final note: So we insert while(true) before our user's sout guess
 //              We enter the break after the line of code where we want the loop to stop
+
 
 
         }
